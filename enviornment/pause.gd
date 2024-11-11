3e2b03f84734cfd4b2e68dc2d842f3754d16108e
Connor <connorheyz@gmail.com>
2024-11-10 20:46:14 -0500

    fixed pause soft lock

--- enviornment/pause.gd
+++ enviornment/pause.gd
@@ -9,7 +9,6 @@ var level_active: Control
 func _ready() -> void:
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
@@ -21,9 +20,4 @@ func _on_resume_button_up() -> void:
 	
 
 func _on_level_select_button_up() -> void:
-	queue_free()
-	var screen: Control = main_screen.instantiate()
-	
-	#screen.main_scene = get_tree()
-	get_tree().root.add_child(screen)
-	level_active = screen
+	get_tree().change_scene_to_file("res://enviornment/level_select.tscn")
